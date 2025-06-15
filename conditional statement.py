0488bb7722956c41e4938f9975344d5379793c92
WON1s <mudathiradan@gmail.com>
2025-06-15 01:06:16 -0700

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,15 @@
+# conditional statement
+# if statement
+
+num=56
+if num>0:
+    print("the number is positive")
+
+else:
+    print("the number is Negative")
+
+num=int(input("enter a number: "))
+if num>15:
+    print("the number is greater than 15")
+else:
+    print("the number is smaller than 15")
